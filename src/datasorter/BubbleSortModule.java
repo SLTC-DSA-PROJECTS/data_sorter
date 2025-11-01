76eaa11dbac4812da5c33f9cfb588bae178154ee
nhjayasundara-web <n.h.jayasundara@gmail.com>
2025-11-02 00:29:51 +0530

    Added my Java project files

--- src/datasorter/BubbleSortModule.java
+++ src/datasorter/BubbleSortModule.java
@@ -2,9 +2,19 @@ package datasorter;
 
 public class BubbleSortModule {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    public static int bubbleSort(int[] arr) {
+	        int steps = 0;
+	        for (int i = 0; i < arr.length - 1; i++) {
+	            for (int j = 0; j < arr.length - i - 1; j++) {
+	                steps++;
+	                if (arr[j] > arr[j + 1]) {
+	                    int temp = arr[j];
+	                    arr[j] = arr[j + 1];
+	                    arr[j + 1] = temp;
+	                }
+	            }
+	        }
+	        return steps;
+	    }
 	}
 
-}
